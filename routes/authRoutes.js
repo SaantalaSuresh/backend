8073f32203c3f13f2e4724a87d7587ca7eff5de3
Suresh <saantalasuresh@gmail.com>
2024-09-05 21:56:54 +0530

    second

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
+
 const jwt = require('jsonwebtoken');
 const User = require("../modules/userSchema");
 
